d7787afef0ced6c29626b722a7db70e39ce9e044
Manuel Vila <mvila@3base.com>
2014-11-17 15:40:48 +0800

    add component.js

---
+++ component.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _ = require('lodash');
+var KindaObject = require('kinda-object');
+
+var Component = KindaObject.extend('Component', function() {
+});
+
+module.exports = Component;
